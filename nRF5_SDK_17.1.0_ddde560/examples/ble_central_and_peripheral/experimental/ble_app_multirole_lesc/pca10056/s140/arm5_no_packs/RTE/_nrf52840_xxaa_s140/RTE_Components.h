cecc2695975043e4ba75414ce4009f3206e0f2ed
wangdayao <bigyao1991@gmail.com>
2026-05-13 16:50:20 +0800

    add the base project

---
+++ nRF5_SDK_17.1.0_ddde560/examples/ble_central_and_peripheral/experimental/ble_app_multirole_lesc/pca10056/s140/arm5_no_packs/RTE/_nrf52840_xxaa_s140/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_multirole_lesc_s140_pca10056' 
+ * Target:  'nrf52840_xxaa_s140' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+#endif /* RTE_COMPONENTS_H */
